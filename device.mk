616c9df51f74b4283d2e29bc2a6eca077b9774cf
Agnes Tachyon <149220283+ruingl@users.noreply.github.com>
2025-11-03 19:46:19 +0800

    Remove bootctrl.mt6765 from PRODUCT_STATIC_BOOT_CONTROL_HAL

--- device.mk
+++ device.mk
@@ -19,9 +19,6 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6765
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6765 \
     libgptutils \
     libz \
