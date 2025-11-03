aec40cf3ebb457ca7cbe2469707055c00ca6844c
Agnes Tachyon <149220283+ruingl@users.noreply.github.com>
2025-11-03 19:45:00 +0800

    Update and rename omni_Infinix-X6525B.mk to twrp_Infinix-X6525B.mk

--- twrp_Infinix-X6525B.mk
+++ twrp_Infinix-X6525B.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from Infinix-X6525B device
 $(call inherit-product, device/infinix/Infinix-X6525B/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6525B
-PRODUCT_NAME := omni_Infinix-X6525B
+PRODUCT_NAME := twrp_Infinix-X6525B
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6525B
 PRODUCT_MANUFACTURER := infinix
